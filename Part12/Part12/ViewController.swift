1f2b81df5cdf1ab2b5a841f6f1a9068d3fda3d9c
higa-ko <85394813+higa-ko@users.noreply.github.com>
2021-08-22 12:46:10 +0900

    Add files via upload

---
+++ Part12/Part12/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Part12
+//
+//  Created by 葡萄酒 on 2021/08/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet private weak var moneyTextField: UITextField!
+    @IBOutlet private weak var taxTextField: UITextField!
+    @IBOutlet private weak var resultLabel: UILabel!
+
+    @IBAction func calculationButton(_ sender: UIButton) {
+        guard let money = Int(moneyTextField.text!) else {return}
+        guard let tax = Int(taxTextField.text!) else {return}
+        
+        let result = money + (money * tax / 100)
+        
+        resultLabel.text = String(result)
+    }
+    
+    @IBAction func changeTaxTextField(_ sender: UITextField) {
+
+        UserDefaults.standard.set(taxTextField.text, forKey: "key")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        taxTextField.text = UserDefaults.standard.string(forKey: "key")
+    }
+}
+
