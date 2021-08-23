2be98223749f1534bb9d0da5e74548d05f375e49
akio0911 <akio0911@gmail.com>
2021-08-23 18:14:56 +0900

    feedback

--- Part12/Part12/ViewController.swift
+++ Part12/Part12/ViewController.swift
@@ -9,28 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let taxKey = "tax"
+
     @IBOutlet private weak var moneyTextField: UITextField!
     @IBOutlet private weak var taxTextField: UITextField!
     @IBOutlet private weak var resultLabel: UILabel!
 
     @IBAction func calculationButton(_ sender: UIButton) {
-        guard let money = Int(moneyTextField.text!) else {return}
-        guard let tax = Int(taxTextField.text!) else {return}
-        
+        guard let money = Int(moneyTextField.text!) else { return }
+        guard let tax = Int(taxTextField.text!) else { return }
+
         let result = money + (money * tax / 100)
-        
+
         resultLabel.text = String(result)
     }
-    
+
     @IBAction func changeTaxTextField(_ sender: UITextField) {
 
-        UserDefaults.standard.set(taxTextField.text, forKey: "key")
+        UserDefaults.standard.set(taxTextField.text, forKey: taxKey)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        taxTextField.text = UserDefaults.standard.string(forKey: "key")
+        taxTextField.text = UserDefaults.standard.string(forKey: taxKey)
     }
 }
-
